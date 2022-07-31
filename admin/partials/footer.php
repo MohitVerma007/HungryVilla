6cc91fcccf6e55e948650a909ee2cd9de60e9bfe
Mohit Verma <108224363+MohitVerma007@users.noreply.github.com>
2022-07-31 18:55:43 +0530

    Update footer.php

--- admin/partials/footer.php
+++ admin/partials/footer.php
@@ -1,7 +1,7 @@
 <!-- Footer Section Starts -->
 <div class="footer">
             <div class="wrapper">
-                <p class="text-center">2020 All rights reserved, Food House. Developed By - <a href="#"> Vijay Thapa</a></p>
+                <p class="text-center">2020 All rights reserved, Food House. Developed By - <a href="#"> Mohit Verma</a></p>
             </div>
         </div>
         <!-- Footer Section Ends -->
